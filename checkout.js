69a8052ce4332237346b259454e8f768837f3872
Megan <carnagh5@gmail.com>
2021-03-11 12:29:23 -0500

    retrieving items from local storage on checkout page

--- checkout.js
+++ checkout.js
@@ -1,6 +1,40 @@
 // // Retrieving items from local storage and adding them to page on page load
-// const retrievedShoppingCart = JSON.parse(localStorage.getItem("cartItem"));
-// console.log(retrievedShoppingCart);
+const loadedCart = JSON.parse(window.localStorage.getItem("cart"));
+console.log(loadedCart);
+
+// On page load, add items from local storage to cart.
+for (let item of loadedCart) {
+  // Create a new div for the item
+  const newDiv = document.createElement("div");
+  //Add a class to newDiv
+  newDiv.classList.add("cartItem__div");
+  // Append the div to the cartItems div
+  document.querySelector(".cartItems__div").appendChild(newDiv);
+  // Create a new image element
+  const newImg = document.createElement("img");
+  // Set the source attribute for the image
+  newImg.setAttribute("src", item.imgSrc);
+  // Add class to newImg
+  newImg.classList.add("cartItem__img");
+  // Add image to the new div
+  newDiv.appendChild(newImg);
+  // Create a subdiv for the name and price
+  const subDiv = document.createElement("div");
+  // Append the sub div to the new div
+  newDiv.append(subDiv);
+  // Create a new h2 for the item name
+  const newh2 = document.createElement("h2");
+  // Set text content for new h2
+  newh2.textContent = item.name;
+  // append new h2 to new div
+  subDiv.appendChild(newh2);
+  // create new h3 for the price
+  const newh3 = document.createElement("h3");
+  // Set text content for new h3
+  newh3.textContent = `Price: $${item.price}`;
+  // append new h2 to new div
+  subDiv.appendChild(newh3);
+}
 
 // function to checkout and reveal payment and input div:
 const checkoutButton = document.getElementById("checkout-button");
