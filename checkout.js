ce539a418baf3cbcf1d9b3adff8ded125e3336f2
Megan <carnagh5@gmail.com>
2021-03-10 22:48:22 -0500

    updated script.js to add items to local storage, updated classes and id's in index.html.

---
+++ checkout.js
@@ -0,0 +1,9 @@
+// function to checkout and reveal payment and input div:
+const checkoutButton = document.getElementById("checkout-button");
+function checkout() {
+  // declare variable for total and input divs here
+  // [name of variable here].display = "block";
+  // declare subtotal variable here
+  // declare sales tax variable here
+  // decalre total variable here
+}
--- script.js
+++ script.js
@@ -1,21 +1,32 @@
+// Select all the Add to Cart buttons
 const addToCartButton = document.querySelectorAll(".item__button");
+// Initialize an empty object for the cart item added
+const cartItem = {};
+// Initialize an empty array for the shopping cart items
+const shoppingCart = [];
 
+// Add event listener to the parent container for the items divs
 document
-  .querySelector("itemsContainer__div")
+  .querySelector(".itemsContainer__div")
   .addEventListener("click", (event) => {
-    console.log(event.target.parentNode.parentNode);
+    // Select the name element of the cart item selected, store in variable
+    const nameElement = event.target.parentNode.parentNode.querySelector(
+      ".itemName__h1"
+    );
+    // Get the text content of the nameElement, store in variable
+    const name = nameElement.textContent;
+    // add the name to the cartItem object
+    cartItem.name = name;
+    // Select the name element of the cart item selected, store in variable
+    const priceElement = event.target.parentNode.querySelector(
+      ".itemPrice__span"
+    );
+    // Get the text content of the priceElement, store in variable
+    const price = priceElement.textContent;
+    // add the price to the cartItem object
+    cartItem.price = price;
+    // Add the cartItem object to the shoppingCart array
+    shoppingCart.push(cartItem);
+    // Add shoppingCart array to local storage
+    localStorage.setItem("cartItem", JSON.stringify(shoppingCart));
   });
-
-// function to checkout and reveal payment and input div:
-const checkoutButton = document.getElementById("checkout-button");
-function checkout() {
-  // declare variable for total and input divs here
-  // [name of variable here].display = "block";
-  // declare subtotal variable here
-  // declare sales tax variable here
-  // decalre total variable here
-}
-
-// function to submit
-
-addToCartButton;
