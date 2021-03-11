55b072b5d71ad2e015fbe210b944257d0bdee0e9
Megan <carnagh5@gmail.com>
2021-03-10 23:08:11 -0500

    added image to shoppingCart array for local storage

--- script.js
+++ script.js
@@ -9,24 +9,34 @@ const shoppingCart = [];
 document
   .querySelector(".itemsContainer__div")
   .addEventListener("click", (event) => {
-    // Select the name element of the cart item selected, store in variable
-    const nameElement = event.target.parentNode.parentNode.querySelector(
-      ".itemName__h1"
-    );
-    // Get the text content of the nameElement, store in variable
-    const name = nameElement.textContent;
-    // add the name to the cartItem object
-    cartItem.name = name;
-    // Select the name element of the cart item selected, store in variable
-    const priceElement = event.target.parentNode.querySelector(
-      ".itemPrice__span"
-    );
-    // Get the text content of the priceElement, store in variable
-    const price = priceElement.textContent;
-    // add the price to the cartItem object
-    cartItem.price = price;
-    // Add the cartItem object to the shoppingCart array
-    shoppingCart.push(cartItem);
-    // Add shoppingCart array to local storage
-    localStorage.setItem("cartItem", JSON.stringify(shoppingCart));
+    if (event.target.classList.contains("item__button")) {
+      // Select the name element of the cart item selected, store in variable
+      const nameElement = event.target.parentNode.parentNode.querySelector(
+        ".itemName__h1"
+      );
+      // Get the text content of the nameElement, store in variable
+      const name = nameElement.textContent;
+      // add the name to the cartItem object
+      cartItem.name = name;
+      // Select the price element of the cart item selected, store in variable
+      const priceElement = event.target.parentNode.querySelector(
+        ".itemPrice__span"
+      );
+      // Get the text content of the priceElement, store in variable
+      const price = priceElement.textContent;
+      // add the price to the cartItem object
+      cartItem.price = price;
+      // Select the image element of the cart item selected, store in variable
+      const imgElement = event.target.parentNode.parentNode.querySelector(
+        ".item__img"
+      );
+      // Get the src attribute of the imgElement, store in variable
+      const imgSrc = imgElement.getAttribute("src");
+      // add the image src to the cartItem object
+      cartItem.imageSource = imgSrc;
+      // Add the cartItem object to the shoppingCart array
+      shoppingCart.push(cartItem);
+      // Add shoppingCart array to local storage
+      localStorage.setItem("cartItem", JSON.stringify(shoppingCart));
+    }
   });
